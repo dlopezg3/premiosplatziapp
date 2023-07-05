5461e6f392d4ff029608c6401135bba8e5ef6c0d
Daniel <dlopez.10g@gmail.com>
2023-07-05 07:59:24 -0500

    avoid hard coding on templates href

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views # del paque polls importame views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     # ex: /polls
-    path("<int:question_id>", views.detail, name="detail"),
+    path("<int:question_id>/Cambio-URL", views.detail, name="detail"),
     # ex: /polls/5
     path("<int:question_id>/results", views.results, name="results"),
     # ex: /polls/5/results
